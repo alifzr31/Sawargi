7f8a266d925830a7ed930dd0d6ff9144eaee7d05
alifzr <alifzakya.r@gmail.com>
2025-04-22 16:03:55 +0700

    first commit

--- lib/app/views/dashboard/dashboard_page.dart
+++ lib/app/views/dashboard/dashboard_page.dart
@@ -12,6 +12,6 @@ class DashboardPage extends StatefulWidget {
 class _DashboardPageState extends State<DashboardPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold();
   }
 }
